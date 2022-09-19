4721cd21192a552e705194712705f02d6767bc8d
Alexander <ad-87@inbox.ru>
2022-09-20 00:10:46 +0300

    выполнил итоговую задачу

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿string[] array = { "hello", "2", "world", ":-)" };
+//string[] array = { "Russia", "Denmark", "Kazan" };
+
+string s = "";
+string sSub = "";
+
+Console.Write("[");
+for (int i = 0; i < array.Length; i++)
+{
+    if (i < array.Length - 1)
+    {
+        Console.Write($"\"{array[i]}\", ");
+    }
+    else
+    {
+        Console.Write($"\"{array[i]}\"] -> ");
+    }
+    if (array[i].Length <= 3)
+    {
+        s = s + array[i] + "|";
+    }
+}
+
+if (s.Length > 2 && s[s.Length - 1] == '|')
+{
+    sSub = s.Substring(0, s.Length - 1);
+}
+
+string[] sArray = sSub.Split('|');
+Console.Write("[");
+
+for (int i = 0; i < sArray.Length; i++)
+{
+    if (i == sArray.Length - 1)
+    {
+        Console.Write($"\"{sArray[i]}\"]");
+    }
+    else
+    {
+        Console.Write($"\"{sArray[i]}\", ");
+    }
+}
