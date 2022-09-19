122b87d1d226efc3731cf2714f3026afcfa0b130
Alexander <ad-87@inbox.ru>
2022-09-20 01:00:49 +0300

    нашел ошибку, поправил

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
-﻿string[] array = { "hello", "2", "world", ":-)" };
-//string[] array = { "Russia", "Denmark", "Kazan" };
+﻿//string[] array = { "hello", "2", "world", ":-)" };
+string[] array = { "Russia", "Denmark", "Kazan" };
 
 string s = "";
 string sSub = "";
@@ -21,22 +21,27 @@ for (int i = 0; i < array.Length; i++)
     }
 }
 
-if (s.Length > 2 && s[s.Length - 1] == '|')
+if (s.Length > 1 && s[s.Length - 1] == '|')
 {
     sSub = s.Substring(0, s.Length - 1);
 }
+else sSub = s;
 
 string[] sArray = sSub.Split('|');
 Console.Write("[");
 
 for (int i = 0; i < sArray.Length; i++)
 {
+    if(sArray.Length == 1) {
+        break;
+    }
     if (i == sArray.Length - 1)
     {
-        Console.Write($"\"{sArray[i]}\"]");
+        Console.Write($"\"{sArray[i]}\"");
     }
-    else
+    else 
     {
         Console.Write($"\"{sArray[i]}\", ");
     }
 }
+Console.Write("]");
